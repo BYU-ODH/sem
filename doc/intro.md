7229aca0ed956fbb399b38d5404a43ae48e4c561
Tory S. Anderson <torys.anderson@gmail.com>
2019-12-11 15:33:27 -0700

    cljsbuild

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to sem
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -5,5 +5,29 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clojure.java-time "0.3.2"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [byu-odh/byu-ws "0.1.6"]]
+  :resource-paths ["target/cljsbuild"]
+  :target-path "target/"
+  :plugins [[lein-cljsbuild "1.1.7"]]
+  :cljsbuild
+  {:builds
+   {:app
+    {:source-paths ["src"]
+     :compiler
+     {:main "sem.core"
+      :asset-path "/js/out"
+      :output-to "target/cljsbuild/public/js/app.js"
+      :output-dir "target/cljsbuild/public/js/out"
+      :optimizations :advanced}}
+    :min
+    {:source-paths ["src"]
+     :compiler
+     {:output-to "target/cljsbuild/public/js/app.js"
+      :output-dir "target/uberjar"
+      :externs ["react/externs/react.js"]
+      :optimizations :advanced
+      :pretty-print false
+      :closure-warnings
+      {:externs-validation :off :non-standard-jsdoc :off}}}}}
   :repl-options {:init-ns sem.core})
