c91ceec0e3819bf5871ca74eec87c87abd052184
Tory S. Anderson <torys.anderson@gmail.com>
2019-12-11 15:36:31 -0700

    v1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject BYU-ODH/sem "0.1.0-SNAPSHOT"
+(defproject BYU-ODH/sem "1"
   :description "BYU Semester helpers"
-  :url ""
+  :url "https://github.com/BYU-ODH/sem"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -8,6 +8,9 @@
                  [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [byu-odh/byu-ws "0.1.6"]]
   :resource-paths ["target/cljsbuild"]
+  :source-paths ["src"]
+  :repositories [["releases" {:url "https://repo.clojars.org"
+                              :creds :gpg}]]
   :target-path "target/"
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild
